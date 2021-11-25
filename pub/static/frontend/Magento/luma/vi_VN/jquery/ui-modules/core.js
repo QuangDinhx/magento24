9d29221f79d1760c9f6400136c7c2d4d14b94400
QuangDinhx <ngquangdinhhzz@gmail.com>
2021-11-25 10:57:52 +0700

    add full project

---
+++ pub/static/frontend/Magento/luma/vi_VN/jquery/ui-modules/core.js
@@ -0,0 +1,20 @@
+// This file is deprecated in 1.12.0 to be removed in 1.13
+( function() {
+define( [
+	"jquery",
+	"./data",
+	"./disable-selection",
+	"./focusable",
+	"./form",
+	"./ie",
+	"./keycode",
+	"./labels",
+	"./plugin",
+	"./safe-active-element",
+	"./safe-blur",
+	"./scroll-parent",
+	"./tabbable",
+	"./unique-id",
+	"./version"
+] );
+} )();
