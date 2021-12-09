574f5e34e28166e54dba88a54b09d31e83cdb86e
lightning dark <pvdung0310@gmail.com>
2021-12-09 10:56:45 +0700

    add 3 module 2 widget

---
+++ app/code/Robin/Kinhdoanhpage/Block/Kinhdoanhpage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Robin\Kinhdoanhpage\Block;
+
+class Kinhdoanhpage extends \Magento\Framework\View\Element\Template
+{
+    protected function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
---
+++ app/code/Robin/Tygiapage/Block/Tygiapage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Robin\Tygiapage\Block;
+
+class Tygiapage extends \Magento\Framework\View\Element\Template
+{
+    protected function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
